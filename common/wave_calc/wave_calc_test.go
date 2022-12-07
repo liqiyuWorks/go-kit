5beac0571b204dc29a9f7c598dc7702de8e94cf0
liqiyu <hamanwendy@outlook.com>
2022-12-07 15:54:03 +0800

    update wave

--- common/wave_calc/wave_calc_test.go
+++ common/wave_calc/wave_calc_test.go
@@ -8,12 +8,11 @@ import (
 
 // 功能测试
 func TestNewWave(t *testing.T) {
-	// seawavedirection:340.72 seawaveheight:1.66 seawaveperiod:6.13
 	degree := 340.72
 	height := 1.66
 	period := 6.13
 	wave := NewWave(degree, height, period)
 	direction := wave.WaveDirection()
 	douglasScale := wave.WaveDouglasScale()
-	base.Glog.Infof("douglasScale=%s, direction=%s", douglasScale, direction,)
+	base.Glog.Infof("douglasScale=%s, direction=%s", douglasScale, direction)
 }
