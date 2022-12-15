81b9d5cf237cc2ce02e65b4df06b43d0ce4da2cd
liqiyu <hamanwendy@outlook.com>
2022-12-15 11:49:42 +0800

    add ConverGfsPointToIndex

--- common/point/point_convert.go
+++ common/point/point_convert.go
@@ -42,3 +42,30 @@ func ConverMfwamPointToIndex(lonPoints, latPoints []string, lon float64, lat flo
 	}
 	return lonIndexs, latIndexs, nil
 }
+
+/**
+ * @description: ConverGfsPointToIndex
+ * @param {*} lonPoints
+ * @param {[]string} latPoints
+ * @param {float64} lon
+ * @param {float64} lat
+ * @return {*}
+ * @author: liqiyuWorks
+ */
+func ConverGfsPointToIndex(lonPoints, latPoints []string, lon float64, lat float64) ([]int, []int, error) {
+	newLonPoints := []float64{}
+	newLatPoints := []float64{}
+	for i := range lonPoints {
+		newLonPoints = append(newLonPoints, base.Decimal2(base.ConvertToFloat64(lonPoints[i], 0)))
+	}
+	for i := range latPoints {
+		newLatPoints = append(newLatPoints, base.ConvertToFloat64(latPoints[i], 0))
+	}
+	lonIndexs, lonErr := BinarySearch(newLonPoints, lon, 0, "asc")
+	latIndexs, latErr := BinarySearch(newLatPoints, lat, 0, "desc")
+	if lonErr != nil || latErr != nil {
+		base.Glog.Errorf("ConverMfwamPointToIndex occur error: lon= %v,lat= %v ", lonErr, latErr)
+		return nil, nil, fmt.Errorf("%v+%v", lonErr, latErr)
+	}
+	return lonIndexs, latIndexs, nil
+}
