126ea164b2797c9d0be9a0c7e015e30839f57046
liqiyu <hamanwendy@outlook.com>
2022-12-23 17:37:37 +0800

    update str

--- base/string_test.go
+++ base/string_test.go
@@ -5,6 +5,12 @@ import (
 	"testing"
 )
 
+/**
+ * @description:
+ * @param {*testing.T} t
+ * @return {*}
+ * @author: liqiyuWorks
+ */
 func TestStringSubA2B(t *testing.T) {
 	str := "liqiyuworks"
 	fmt.Println(StringSubA2B(str, 1, 3, true))
