93465c95c005eda445219b86049efa2b998c5160
liqiyu <hamanwendy@outlook.com>
2023-01-06 11:07:35 +0800

    update statuscode error code

--- base/statuscode/http_code.go
+++ base/statuscode/http_code.go
@@ -1,10 +1,10 @@
 /*
  * @Author: lisheng
  * @Date: 2022-11-02 22:59:39
- * @LastEditTime: 2022-11-03 00:48:23
+ * @LastEditTime: 2023-01-06 11:07:08
  * @LastEditors: lisheng
  * @Description: http状态码
- * @FilePath: /gitee.com/liqiyuworks/jf-go-kit/base/statuscode/http_code.go
+ * @FilePath: /jf-go-kit/base/statuscode/http_code.go
  */
 package statuscode
 
@@ -14,6 +14,6 @@ type Status struct {
 }
 
 var (
-	OK            = &Status{1, "请求成功"}
-	ERROR_DEFAULT = &Status{-1, "请求失败"}
+	OK    = &Status{1, "请求成功"}
+	ERROR = &Status{-1, "请求失败"}
 )
